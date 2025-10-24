bcd0e42bab381aa123e739eaf28eeddf0ecb36e3
Saad711T <75455552+Saad711T@users.noreply.github.com>
2025-10-24 14:38:15 +0300

    Added basic commands lecture

---
+++ Linux-Commands/basiccommands.sh
@@ -0,0 +1,31 @@
+# Basic commands appears on pdf file :
+ls
+
+ls -l
+
+ls -la
+
+
+
+mkdir test
+
+cd test
+
+cd -
+
+cd test
+
+cd ..
+
+
+rmdir test
+
+
+mkdir -p tesst/test1/test2
+
+rm -r tesst
+
+
+touch main.txt
+
+nano main.txt
